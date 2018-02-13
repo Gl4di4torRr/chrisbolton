f46d7aa2c85dc1fc4497f8a093aa8685d6aae60b
Chris <Bolton@xatlmac0070smbp.delta.rl.delta.com>
2018-02-13 15:26:22 -0500

    add ip back to gulp serveprod

--- gulpfile.js
+++ gulpfile.js
@@ -90,6 +90,7 @@ gulp.task('serveprod', function() {
     connect.server({
         root: './',
         port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        ip: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
         livereload: false
     });
 });
